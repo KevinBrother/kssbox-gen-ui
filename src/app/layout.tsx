18fbc392ab472583604d91bd4c2f32afe09fb9a1
kevinbrother <1301239018@qq.com>
2024-06-29 15:57:06 +0800

    feat(monaco-editor)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "./header";
-import { CodeEditor } from "./code-editor";
 
 const inter = Inter({ subsets: ["latin"] });
 
